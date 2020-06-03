b8dd02655615dc2d1599097de7621ab8a71c432f
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-03 19:09:09 +0900

    props/state, 그리고 key에 대한 개념 공부

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 
 class TOC extends Component {
   render() {
+    var lists = [];
+    var data = this.props.data
+    var i = 0;
+    while(i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a 
+            href={"/content/"+data[i].id}
+          >
+            {data[i].title}
+          </a>
+        </li>
+      )
+      i += 1;
+    }
+
     return (
       <nav>
         <ul>
-          <li><a href="#">HTML</a></li>
-          <li><a href="#">CSS</a></li>
-          <li><a href="#">Javascript</a></li>
+          {lists}
         </ul>
       </nav>
     );
