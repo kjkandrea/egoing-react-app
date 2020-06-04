3632cf95d14c97e3a50635d505856970af7fb682
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-04 15:29:14 +0900

    state 업데이트에 대한 공부

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
+      mode: 'read',
       subject: {
         title: 'WEB',
         sub: 'World Wide Web!'
+      },
+      welcome: {
+        title: 'Welcome',
+        desc: 'Hello, React!!'
       },
       contents: [
         {
@@ -31,17 +36,42 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title
+      _desc = this.state.welcome.desc
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title
+      _desc = this.state.contents[0].desc
+    }
     return (
       <div className="App">
-        <Subject 
-          title={this.state.subject.title} sub={this.state.subject.sub}
-        />
+        {/* <Subject 
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        /> */}
+        <header>
+          <h1>
+            <a
+              href="/" 
+              onClick={function(e){
+                e.preventDefault()
+                this.setState({
+                  mode : 'welcome'
+                })
+              }.bind(this)}
+            >
+            {this.state.subject.title}
+          </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC
           data={this.state.contents}
         />
         <Content 
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
+          title={_title}
+          desc={_desc}
         />
       </div>
     );
